03e4ca948a5c6f1115d628801f26fe7b85eef7f8
Aqsa <161750279+Aqsa-owais@users.noreply.github.com>
2026-01-11 00:30:32 +0500

    first commit

--- components/pages/assignments.tsx
+++ components/pages/assignments.tsx
@@ -179,6 +179,34 @@ export default function Assignments() {
                   Submit Assignment
                 </Button>
               </div>
+              {/* 6th Assignments */}
+               <div className="flex flex-col sm:flex-row gap-3 pt-4 border-t border-border">
+                <Button
+                  variant="outline"
+                  className="flex items-center gap-2"
+                  onClick={() =>
+                    window.open(
+                      "https://docs.google.com/document/d/1pkMC-F3sTA4kiwFwx2RbZ1myuIgRU5IkgzaB6GWaG9A/edit?tab=t.0#heading=h.olpw68ve72fd",
+                      "_blank"
+                    )
+                  }
+                >
+                  <FileText size={18} />
+                  Assignment Instructions
+                </Button>
+                <Button
+                  className="flex items-center gap-2 bg-primary hover:bg-primary/90 text-primary-foreground"
+                  onClick={() =>
+                    window.open(
+                      "https://docs.google.com/forms/d/e/1FAIpQLSdB3C5lRjDh8InISos24NoB8wNLZqhCwp0NEt1q2xG3eDuAvA/viewform",
+                      "_blank"
+                    )
+                  }
+                >
+                  <LinkIcon size={18} />
+                  Submit Assignment
+                </Button>
+              </div>
             </div>
           ))}
         </div>
